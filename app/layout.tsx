14d5968475943fad3308ec71aa2fefce8f764089
wangxiao <wangxiao@didachuxing.com>
2023-10-08 13:56:34 +0800

    08102023 同步更新main代码-Yidadaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Oh My IdeaTalk",
-  description: "Your personal ChatGPT Chat Bot.",
+  description: "Your personal Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
